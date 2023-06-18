7f52428d4d501e0b79a60c2a5ae62943d16a78b8
Ajay Kumar <ajaymahto20319@gmail.com>
2023-06-18 10:06:41 +0530

    Blog

--- src/main/java/com/driver/services/BlogService.java
+++ src/main/java/com/driver/services/BlogService.java
@@ -23,11 +23,20 @@ public class BlogService {
 
     public Blog createAndReturnBlog(Integer userId, String title, String content) {
         //create a blog at the current time
+        Blog blog = new Blog();
+        User user = userRepository1.findById(userId).get();
+        blog.setUser(user);
+        blog.setTitle(title);
+        blog.setContent(content);
+        blogRepository1.save(blog);
 
+        return blog;
     }
 
     public void deleteBlog(int blogId){
         //delete blog and corresponding images
-
+        Blog blog = blogRepository1.findById(blogId).get();
+        List<Image> imageList = blog.getImageList();
+        blogRepository1.delete(blog);
     }
 }
--- src/main/java/com/driver/services/ImageService.java
+++ src/main/java/com/driver/services/ImageService.java
@@ -17,15 +17,29 @@ public class ImageService {
 
     public Image addImage(Integer blogId, String description, String dimensions){
         //add an image to the blog
+        Blog blog = blogRepository2.findById(blogId).get();
+        Image image = new Image();
+        image.setDescription(description);
+        image.setDimensions(dimensions);
+        blog.getImageList().add(image);
+        blogRepository2.save(blog);
+
+        return image;
 
     }
 
     public void deleteImage(Integer id){
-
+        imageRepository2.deleteById(id);
     }
 
     public int countImagesInScreen(Integer id, String screenDimensions) {
         //Find the number of images of given dimensions that can fit in a screen having `screenDimensions`
+        Image image = imageRepository2.findById(id).get();
+        String dimen = image.getDimensions();
 
+        int a = dimen.charAt(0) * dimen.charAt(2);
+        int b = screenDimensions.charAt(0) * screenDimensions.charAt(2);
+
+        return b / a;
     }
 }
